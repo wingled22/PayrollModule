853ad0c3958e187421ea3ef98d2248fa13b05aa2
wingled22 <pelayowindel@gmail.com>
2022-01-15 13:25:14 +0800

    ...

---
+++ Report/PayrollReport.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace PayrollModule.Report
+{
+    partial class PayrollReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource3 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.GetPayrollFromPayrollPeriod_ResultBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.GetPayrollFromPayrollPeriod_ResultBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource3.Name = "DataSet1";
+            reportDataSource3.Value = this.GetPayrollFromPayrollPeriod_ResultBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource3);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "PayrollModule.Report.Payroll.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(1046, 450);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // GetPayrollFromPayrollPeriod_ResultBindingSource
+            // 
+            this.GetPayrollFromPayrollPeriod_ResultBindingSource.DataSource = typeof(PayrollModule.Entities.GetPayrollFromPayrollPeriod_Result);
+            // 
+            // PayrollReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1046, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "PayrollReport";
+            this.Text = "PayrollReport";
+            this.Load += new System.EventHandler(this.PayrollReport_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GetPayrollFromPayrollPeriod_ResultBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource GetPayrollFromPayrollPeriod_ResultBindingSource;
+    }
+}
